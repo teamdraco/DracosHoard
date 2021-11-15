a6ed3e480a0e146c63280ee360f327fd3e9daadf
Coda <cody.koelliker@outlook.com>
2021-11-14 21:52:27 -0700

    add armor material & item tier

---
+++ src/main/java/coda/weecore/common/items/WCItemTier.java
@@ -0,0 +1,54 @@
+package coda.weecore.common.items;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum WCItemTier implements IItemTier {
+    EXAMPLE_TIER(2, 150, 6.0F, 2.0F, 25, () -> {
+        return Ingredient.of(Items.OBSIDIAN);
+    });
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    private WCItemTier(int p_i48458_3_, int p_i48458_4_, float p_i48458_5_, float p_i48458_6_, int p_i48458_7_, Supplier<Ingredient> p_i48458_8_) {
+        this.level = p_i48458_3_;
+        this.uses = p_i48458_4_;
+        this.speed = p_i48458_5_;
+        this.damage = p_i48458_6_;
+        this.enchantmentValue = p_i48458_7_;
+        this.repairIngredient = new LazyValue<>(p_i48458_8_);
+    }
+
+    public int getUses() {
+        return this.uses;
+    }
+
+    public float getSpeed() {
+        return this.speed;
+    }
+
+    public float getAttackDamageBonus() {
+        return this.damage;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
+
+    public int getEnchantmentValue() {
+        return this.enchantmentValue;
+    }
+
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
